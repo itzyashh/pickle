13ca39ac695a3c0384c0daf1aeae0aa0c4ff9c9d
Yash Jadhav <yashjadhav1411@rocketmail.com>
2023-08-30 20:54:57 +0530

    add fonts and localization

---
+++ src/constants/lang/index.js
@@ -0,0 +1,10 @@
+import en from "./en";
+import ar from "./ar";
+import LocalizedStrings from "react-native-localization";
+
+let strings = new LocalizedStrings({
+    en: en,
+    ar: ar,
+});
+
+export default strings;
