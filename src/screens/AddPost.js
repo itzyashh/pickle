e969cc6173a64d0bf032b8dd82e303e87a0a10b7
Yash Jadhav <yashjadhav1411@rocketmail.com>
2023-11-01 03:30:44 +0530

    Update AddPost.js

--- src/screens/AddPost.js
+++ src/screens/AddPost.js
@@ -3,13 +3,37 @@ import React from 'react'
 import WrapperComponent from '../components/WrapperComponent'
 import Header from '../components/Header'
 import strings from '../constants/lang'
-import { width } from '../assets/scaling'
+import { moderateScale, width } from '../assets/scaling'
+import colors from '../constants/colors'
+import { useSelector } from 'react-redux'
+import ImageCropPicker from 'react-native-image-crop-picker'
+import { showMessage } from 'react-native-flash-message'
 
 const AddPost = ({navigation,route}) => {
+  const {isDark,language} = useSelector(state => state?.appSettings)
 
 const [images, setImages] = React.useState(route.params.selectedImages || []);
 
-console.log('images++', images);
+const onPlus = () => {
+
+  if(images.length >= 4) {
+    return showMessage({
+      message: 'You can select maximum 4 images',
+      type: 'danger',
+      icon: 'danger'
+    })
+  }
+
+  ImageCropPicker.openPicker({
+    width: 300,
+    height: 400,
+    multiple: true,
+  }).then(i => {
+    setImages([...images,...i])
+  }).catch(err => {
+    console.log(err);
+  });
+}
 
 const renderItem = ({item,index}) => {
 
@@ -24,12 +48,14 @@ const renderItem = ({item,index}) => {
   return (
     <TouchableOpacity
       // onPress={}
+      style={{marginRight: moderateScale(10)}}
     >
       <Image
         source={{uri: item?.node?.image?.uri || item?.path }}
         style={{
           width: width / 4,
           height: width / 4,
+          borderRadius: moderateScale(10)
         }}
       />
       {/* {
@@ -52,13 +78,28 @@ const renderItem = ({item,index}) => {
   return (
     <WrapperComponent>
         <Header showTitle title={strings.ADD_INFO} />
+        <View
+          style={{padding: moderateScale(16)}}>
         <FlatList
           data={images}
           renderItem={renderItem}
           horizontal
           showsHorizontalScrollIndicator={false}
           keyExtractor={(item, index) => index.toString()}
+          ListFooterComponent={<TouchableOpacity onPress={onPlus} style={{
+            width: width / 4,
+            height: width / 4,
+            borderRadius: moderateScale(10),
+            backgroundColor: colors.gray,
+            justifyContent: 'center',
+            alignItems: 'center'
+          }}>
+            <Text style={{fontSize: moderateScale(30),color:isDark ? colors.white : colors.black }}>+</Text>
+          </TouchableOpacity>
+
+          }
         />
+        </View>
     </WrapperComponent>
   )
 }
